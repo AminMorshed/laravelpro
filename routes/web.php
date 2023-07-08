301f0602dccbcb51b3baddb5762c1936d450e9bc
Amin <aminmorshed2020@gmail.com>
2023-07-08 18:07:45 +0330

    Creating a user panel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Auth\GoogleAuthController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use RealRashid\SweetAlert\Facades\Alert;
 
@@ -21,15 +23,21 @@ Route::get('/', function () {
 });
 
 
-
 Auth::routes(['verify' => true]);
 
-Route::prefix('auth/')->group(function (){
-    Route::get('google',[GoogleAuthController::class , 'redirect'])->name('auth.google');
-    Route::get('google/callback',[GoogleAuthController::class , 'callback']);
+Route::prefix('auth/')->group(function () {
+    Route::get('google', [GoogleAuthController::class, 'redirect'])->name('auth.google');
+    Route::get('google/callback', [GoogleAuthController::class, 'callback']);
 });
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/secret', function (){
-   return 'secret';
-})->middleware(['auth' , 'password.confirm']);
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/secret', function () {
+    return 'secret';
+})->middleware(['auth', 'password.confirm']);
+
+Route::prefix('profile/')->group(function () {
+    Route::get('', [ProfileController::class, 'index'])->name('profile');
+    Route::get('twofactor', [ProfileController::class, 'manageTwoFactor'])->name('profile.2fa.manage');
+});
+
+
